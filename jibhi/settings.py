df80acc694f8f4e80f51cb38582b2f48cf132e30
tejender <kumartejender007@gmail.com>
2022-12-22 17:32:33 +0530

    after working commit

--- jibhi/settings.py
+++ jibhi/settings.py
@@ -35,13 +35,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',    
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic',   
+    # 'whitenoise.runserver_nostatic',   
     'listings.apps.ListingsConfig'    
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,18 +119,39 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+####------------for development----------------------##########
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    BASE_DIR / 'static'
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_ROOT = os.path.join (BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join (BASE_DIR, 'media')
+
+#####--------------------------##############################
+
+#### for production--------------------------#############
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'staticfiles')
+# ]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join (BASE_DIR, 'media')
+########---------------------------##################
 
 
 # Default primary key field type
