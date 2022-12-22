d72e310f190753917a0a71eb80a947c3b3498078
tejender <kumartejender007@gmail.com>
2022-12-22 17:35:27 +0530

    after working commit-1

--- jibhi/settings.py
+++ jibhi/settings.py
@@ -35,13 +35,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',    
     'django.contrib.staticfiles',
-    # 'whitenoise.runserver_nostatic',   
+    'whitenoise.runserver_nostatic',   
     'listings.apps.ListingsConfig'    
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # "whitenoise.middleware.WhiteNoiseMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
